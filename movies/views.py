81221c8b89ac9a15d32246fb3da2d2cba8ccd520
avosa <websterb17@gmail.com>
2020-11-09 16:28:22 +0300

    Initial commit

---
+++ movies/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Movie
+
+# Create your views here.
+
+
+def index(request):
+    movies = Movie.objects.all()  # same as SELECT * FROM Movies_movie
+
+    return render(request, 'movies/index.html', {'movies': movies})
+
+
+def detail(request, movie_id):
+    movie = Movie.objects.get(pk=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
